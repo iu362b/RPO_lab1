466bc44371abc52eb66ca0e344769322e3ab4f0b
iu362b <101719965+iu362b@users.noreply.github.com>
2022-04-28 20:56:33 +0300

    ЛР2 и ЛР3

--- Yenshin Bogdan/app/src/main/java/com/example/lab/MainActivity.java
+++ Yenshin Bogdan/app/src/main/java/com/example/lab/MainActivity.java
@@ -1,18 +1,42 @@
 package com.example.lab;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+import org.apache.commons.io.IOUtils;
 
 
 import com.example.lab.databinding.ActivityMainBinding;
 
-import java.nio.charset.StandardCharsets;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.text.DecimalFormat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class MainActivity extends AppCompatActivity implements TransactionEvents {
+
 
     // Used to load the 'lab' library on application startup.
+    private String pin;
+    private ActivityResultLauncher activityResultLauncher;
 
     static {
         System.loadLibrary("lab");
@@ -20,6 +44,83 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private ActivityMainBinding binding;
+
+    public static byte[] stringToHex(String s)
+    {
+        byte[] hex;
+        try
+        {
+            hex = Hex.decodeHex(s.toCharArray());
+        }
+        catch (DecoderException ex)
+        {
+            hex = null;
+        }
+        return hex;
+    }
+
+    protected void testHttpClient()
+    {
+        new Thread(() -> {
+            try {
+                HttpURLConnection uc = (HttpURLConnection)
+                        (new URL("http://192.168.80.163:8081/api/v1/title").openConnection());
+
+                InputStream inputStream = uc.getInputStream();
+                String html = IOUtils.toString(inputStream);
+                String title = getPageTitle(html);
+                runOnUiThread(() ->
+                {
+                    Toast.makeText(this, title, Toast.LENGTH_LONG).show();
+                });
+            } catch (Exception ex) {
+                Log.e("fapptag", "Http client fails", ex);
+            }
+        }).start();
+    }
+
+    protected String getPageTitle(String html)
+    {
+        Pattern pattern = Pattern.compile("<title>(.+?)</title>", Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(html);
+        String p;
+        if (matcher.find())
+            p = matcher.group(1);
+        else
+            p = "Not found";
+        return p;
+    }
+
+    public void onButtonClick(View v)
+    {
+        //byte[] trd = stringToHex("9F0206000000050000");
+        //boolean ok = transaction(trd);
+        testHttpClient();
+    }
+
+    @Override
+    public String enterPin(int ptc, String amount) {
+        pin = new String();
+        Intent it = new Intent(MainActivity.this, PinpadActivity.class);
+        it.putExtra("ptc", ptc);
+        it.putExtra("amount", amount);
+        synchronized (MainActivity.this) {
+            activityResultLauncher.launch(it);
+            try {
+                MainActivity.this.wait();
+            } catch (Exception ex) {
+//todo: log error
+            }
+        }
+        return pin;
+    }
+
+    @Override
+    public void transactionResult(boolean result) {
+        runOnUiThread(()-> {
+            Toast.makeText(MainActivity.this, result ? "ok" : "failed", Toast.LENGTH_SHORT);
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,17 +131,38 @@ public class MainActivity extends AppCompatActivity {
 
         int res = initRng();
         byte[] v = randomBytes(10);
-        byte[] key  = "AB093ASM300T5Y67".getBytes(StandardCharsets.UTF_8);
-        byte[] data = "___HELLO_LABS___".getBytes(StandardCharsets.UTF_8);
-        byte[] encripted = encrypt(key, data);
-        byte[] decripted = decrypt(key, encripted);
-
-        String str = new String(decripted, StandardCharsets.UTF_8);
 
 
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        activityResultLauncher  = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        // обработка результата
+//                        assert data != null;
+//                        String pin = data.getStringExtra("pin");
+//                        Toast.makeText(MainActivity.this, pin,
+//                                Toast.LENGTH_SHORT).show();
+                        pin = data.getStringExtra("pin");
+                        synchronized (MainActivity.this) {
+                            MainActivity.this.notifyAll();
+                        }
+                    }
+                });
+
+
+
+//        byte[] key  = "AB093ASM300T5Y67".getBytes(StandardCharsets.UTF_8);
+//        byte[] data = "___HELLO_LABS___".getBytes(StandardCharsets.UTF_8);
+//        byte[] encripted = encrypt(key, data);
+//        byte[] decripted = decrypt(key, encripted);
+//
+//        String str = new String(decripted, StandardCharsets.UTF_8);
+
+
+        //Example of a call to a native method
+        //TextView tv = findViewById(R.id.sample_text);
+        //tv.setText(stringFromJNI());
 
     }
 
@@ -52,5 +174,6 @@ public class MainActivity extends AppCompatActivity {
     public static native byte[] randomBytes(int no);
     public static native byte[] encrypt(byte[] key, byte[] data);
     public static native byte[] decrypt(byte[] key, byte[] data);
+    public native boolean transaction(byte[] trd);
     public native String stringFromJNI();
 }
---
+++ Yenshin Bogdan/app/src/main/java/com/example/lab/TransactionEvents.java
@@ -0,0 +1,7 @@
+package com.example.lab;
+
+public interface TransactionEvents {
+        String enterPin(int ptc, String amount);
+        void transactionResult(boolean result);
+}
+
