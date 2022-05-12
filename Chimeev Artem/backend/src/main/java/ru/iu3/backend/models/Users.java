bd29da31d6b2a931cb83bb10a83bdb4b775cd35e
Gotica0 <pspart.2001@mail.ru>
2022-05-12 16:33:51 +0300

    Добавил ЛР1-12

---
+++ Chimeev Artem/backend/src/main/java/ru/iu3/backend/models/Users.java
@@ -0,0 +1,73 @@
+package ru.iu3.backend.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Access(AccessType.FIELD)
+public class Users {
+    // ID - первинчый ключ
+    @Id
+    @Column(name = "id", updatable = false, nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    // Поле логина. Имеет тип VARCHAR. Для нас это String
+    @Column(name = "login", unique = true, nullable = false)
+    public String login;
+
+    // Ну всё-таки скрыть пароль надо бы)
+    @JsonIgnore
+    @Column(name = "password")
+    public String password;
+
+    // Поле - электронная почта
+    @Column(name = "email")
+    public String email;
+
+    // Поле - соль
+    @JsonIgnore
+    @Column(name = "salt")
+    public String salt;
+
+    // Поле - токен
+
+    @Column(name = "token")
+    public String token;
+
+    // Поле активности
+    @Column(name = "activity")
+    public LocalDateTime activity;
+
+    // NEW!!!
+    // Добавляем новое поле - новый пароль
+    @Transient
+    public String np;
+
+    // Устанавливаем отношение многим-ко-многим
+    // Важно: для отношений многие-ко-многим нужно использовать именно множество, потому что
+    // JPA генерирует очень неэффективный код
+    @ManyToMany(mappedBy = "users")
+    public Set<Museum> museums = new HashSet<>();
+
+    public Users() {}
+
+    public Users(Long id) {
+        this.id = id;
+    }
+
+    public void addMuseum(Museum m) {
+        this.museums.add(m);
+        m.users.add(this);
+    }
+
+    public void removeMuseum(Museum m) {
+        this.museums.remove(m);
+        m.users.remove(this);
+    }
+}
