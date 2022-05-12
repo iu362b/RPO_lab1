926638370e54af488a19af6f318118f96399b8c8
Gotica0 <pspart.2001@mail.ru>
2022-05-12 16:16:32 +0300

    Добавил 1-12Lab

---
+++ Chimeev Artem/backend/src/main/java/ru/iu3/backend/repositories/UsersRepository.java
@@ -0,0 +1,13 @@
+package ru.iu3.backend.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.iu3.backend.models.Users;
+
+import java.util.Optional;
+
+public interface UsersRepository extends JpaRepository<Users, Long> {
+    // Методы можно плодить таким образом: нет никакой реализации, но при этом на уровне определения JAVA понимает
+    // что нужно обращаться к конкретному полю
+    Optional<Users> findByToken(String valueOf);
+    Optional<Users> findByLogin(String login);
+}
